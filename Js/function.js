d73e20c6ae7e7121b1e409d457058524b93bd4dc
albertdevop <betomartinez3602@gmail.com>
2023-02-24 16:37:42 -0300

    Primer repo de cv

---
+++ Js/function.js
@@ -0,0 +1,9 @@
+document.getElementById("send").onclick=function() {
+    document.getElementById("ok-sended").innerHTML="Su mensaje ha sido enviado"
+}
+
+
+document.getElementById("boton").onclick=function() {
+    document.getElementById("info").innerHTML=href="https://github.com/albertdevop/betoprojects.git"
+
+}
